69994a18507781fe0049fcf02cb734a03c04873e
Rob Sutter <rts-rob@users.noreply.github.com>
2018-09-01 17:55:07 +0400

    Serverless-ed the Nuxt rendering.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,46 +1,36 @@
-const { API_ROOT, I18N } = require('./config')
+const { API_ROOT, I18N } = require("./config");
 
 module.exports = {
-  /*
-  ** Headers of the page
-  */
+  mode: "universal",
   head: {
-    title: 'nuxt-i18n-example',
+    title: "nuxt-i18n-example",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Nuxt.js project" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Build configuration
-  */
+  loading: { color: "#fff" },
   build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, ctx) {
-      if (ctx.dev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
+    vendor: ["axios"],
+    publicPath: `/${require("./secrets.json").NODE_ENV}/_nuxt/`
   },
+  // srcDir: "./",
+  performance: {
+    gzip: false
+  },
+  router: {
+    base: `/`
+  },
+  dev: false,
   modules: [
-    ['nuxt-i18n', I18N],
-    ['@nuxtjs/axios', {
-      baseURL: API_ROOT
-    }]
+    ["nuxt-i18n", I18N],
+    [
+      "@nuxtjs/axios",
+      {
+        baseURL: API_ROOT
+      }
+    ]
   ]
-}
+};
